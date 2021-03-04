a9e1341857dfdd753c262e5449e6b3c42067223b
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2021-03-04 13:18:47 -0300

    Update Methods.java

--- src/part_2_methods/Methods.java
+++ src/part_2_methods/Methods.java
@@ -21,7 +21,41 @@ public class Methods { // this is the class
         System.out.println("hello, from a method.");
     }
 
+    static void secoundMethod(String firstName) {
+        System.out.println(firstName + ", the 'parameter' is working");
+    }
+
+    static void thirdMethod(String hostname, int ip) {
+        /*
+        Note that when you are working with multiple parameters,
+        the method call must have the same number of arguments as
+        there are parameters, and the arguments must be passed in
+        the same order.
+        */
+        System.out.println("hostname: " + hostname + ", ip: " + ip);
+    }
+
+    static int forthMethod(int x) {
+        /*
+        Return Values
+        The void keyword, used in the examples above, indicates that 
+        the method should not return a value. If you want the method
+        to return a value, you can use a primitive data type (such 
+        as int, char, etc.) instead of void, and use the return 
+        keyword inside the method:
+        */
+        return x + 10;
+    }
+
+    static int fifthMethod(int y, int z) {
+        return(y + z);
+    }
     public static void main(String[] args) {
-        firstMethod();
+        firstMethod(); // calling the method in the main method
+        secoundMethod("dear sir"); // parameter is passed to method
+        secoundMethod("dear ma'am"); 
+        thirdMethod("admin", 123); // multiple parameters
+        System.out.println(forthMethod(10));
+        System.out.println(fifthMethod(10,5));
       }
 }
